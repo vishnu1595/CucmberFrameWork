f98082e61d25d5a108548edc4033a8c8cd94efd6
vishnuraj16 <vikrantraj15@gmail.com>
2022-01-30 00:11:36 +0530

    changes done in pom.xml

--- CucumberPOMSeries/test-output-thread/report.js
+++ CucumberPOMSeries/test-output-thread/report.js
@@ -1,43 +1,43 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "c6b3c42f-bb0b-4fe3-a76a-29d620a3ccc3",
-    "feature": "Login page feature",
-    "scenario": "Login with correct credentials",
-    "start": 1643454720501,
-    "group": 15,
-    "content": "",
-    "tags": "@login,@regression,",
-    "end": 1643454726634,
-    "className": "passed"
-  },
-  {
-    "id": "2a4b7f9f-abb0-46bd-be69-a7f2d523230a",
+    "id": "70f63595-5835-472f-9018-f98f4296516a",
     "feature": "Login page feature",
     "scenario": "Login page title",
-    "start": 1643454713630,
-    "group": 15,
+    "start": 1643481621282,
+    "group": 1,
     "content": "",
     "tags": "@login,",
-    "end": 1643454718154,
+    "end": 1643481626224,
     "className": "passed"
   },
   {
-    "id": "59afc652-7fa7-4296-87bb-d7be8fc9add1",
+    "id": "7b5659c2-5a29-4311-b373-bf71f450cf9c",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1643481629488,
+    "group": 1,
+    "content": "",
+    "tags": "@login,",
+    "end": 1643481635851,
+    "className": "passed"
+  },
+  {
+    "id": "979e0916-f79c-42ef-80c1-624d41c05521",
     "feature": "Login page feature",
     "scenario": "Forgot Password link",
-    "start": 1643454718179,
-    "group": 15,
+    "start": 1643481626245,
+    "group": 1,
     "content": "",
-    "tags": "@login,@skip,",
-    "end": 1643454720486,
-    "className": "skipped"
+    "tags": "@login,",
+    "end": 1643481629475,
+    "className": "passed"
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
   {
-    "id": 15,
-    "content": "Thread[pool-1-thread-1,5,main]"
+    "id": 1,
+    "content": "Thread[main,5,main]"
   }
 ]);
 });
